93ed311a01f563d2dbbda62a59d7421a5559ddbd
sikutisa <ilgooinkorea@gmail.com>
2024-03-04 17:04:40 +0900

    Chore: add backend boiler plate
    Related to: S10P22A104-9

---
+++ backend/src/main/java/io/watssuggang/voda/VodaApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:397ae45f09d3190f0ec82fef6b5380a7183c5193d3343cb344c9b47501008664
+size 308
---
+++ backend/src/test/java/io/watssuggang/voda/VodaApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfa28b99d772b53065e24053d177e64fa1066da8ec874904c5c1d542eda99c3e
+size 209
