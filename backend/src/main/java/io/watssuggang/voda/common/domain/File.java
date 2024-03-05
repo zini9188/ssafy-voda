96202f5fffd62bcaacc03214ccb5415753a9e443
sikutisa <ilgooinkorea@gmail.com>
2024-03-05 16:57:26 +0900

    Chore: add File entity
    Related to: S10P22A104-53

---
+++ backend/src/main/java/io/watssuggang/voda/common/domain/File.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9cfa8c196ac860b32e5cbf1dab7e02c510c287b997fdf6bf263fb052420defb7
+size 654
---
+++ backend/src/main/java/io/watssuggang/voda/common/enums/FileType.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:30b27c78d660dd09cb87a12dabb2f96629629f72b6292cd0095bb74014e73adc
+size 127
