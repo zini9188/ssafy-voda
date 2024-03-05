b614b6dd6f8ee797c7c5b7e63ed2d9a8cb2667e4
sikutisa <ilgooinkorea@gmail.com>
2024-03-05 16:29:31 +0900

    Chore: add BaseEntity
    Related to: S10P22A104-9, S10P22A104-38

--- backend/src/main/java/io/watssuggang/voda/VodaApplication.java
+++ backend/src/main/java/io/watssuggang/voda/VodaApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:397ae45f09d3190f0ec82fef6b5380a7183c5193d3343cb344c9b47501008664
-size 308
+oid sha256:a3fcc07591e154a8fb9f723aee7bb219d7ae37612b3c7908cf3c30151d4cc673
+size 412
