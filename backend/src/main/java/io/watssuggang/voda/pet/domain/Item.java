6533384de421972e0666244db3f143cf2de0b429
sikutisa <ilgooinkorea@gmail.com>
2024-03-06 12:08:32 +0900

    fix: add constructor access level

--- backend/src/main/java/io/watssuggang/voda/pet/domain/Item.java
+++ backend/src/main/java/io/watssuggang/voda/pet/domain/Item.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43b62fab182fa12c6d6a2abd1557cdd521717ca71c6bd97561f5f7f98bc028fd
-size 1570
+oid sha256:4be2a5bbebbe48783583c88fdfbca29e35ea038020bbb4bbab9967f690494072
+size 1629
--- backend/src/main/java/io/watssuggang/voda/pet/domain/PetFile.java
+++ backend/src/main/java/io/watssuggang/voda/pet/domain/PetFile.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7accf19b0e0a7b25da81141b9ca75bb33b649849609dfd491f4e9a0c7cb41add
-size 652
+oid sha256:a8e35e52388f5682464b792a01ba535ca75f377df70b51063356fe3312c0f868
+size 741
