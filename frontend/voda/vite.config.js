bd399810dad09bce2a54445fbde3e450dd9fdef3
이수민 <suminjeff@gmail.com>
2024-03-08 00:12:59 +0900

    chore: Added new voda icons

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27dd1ddb716cbb4592b6a9180eeca0220c73a9d1ce71f93eb0e3737b664bb6fe
-size 1069
+oid sha256:feaf752a62adce48a4e9e3d879769e6e18da3ef4e9ba94c29fa0165424e3eee2
+size 1058
