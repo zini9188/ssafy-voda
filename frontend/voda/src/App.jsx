b06906a941f1ed59c8ea6f2867efc2a00d87bdd8
김병주 <qudwn2941@naver.com>
2024-03-07 09:43:09 +0900

    Feat: Set the router structure
    Related to: S10P22A104-23

--- frontend/voda/src/App.jsx
+++ frontend/voda/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb5e79cc137ae3c4f14402949c1af7754ad80d0d3139b338e3d537240138f965
-size 149
+oid sha256:ced720aabaf879d62e405e7890f37d2182e44710981e52e2eedce2f903b59f28
+size 1455
