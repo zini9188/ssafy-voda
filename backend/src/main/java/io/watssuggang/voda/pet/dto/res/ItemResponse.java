8ede42273f99855fe9bfc18e9bd52f0b248e4c23
zqwz <zqwz@naver.com>
2024-03-08 11:06:07 +0900

    fix: item 아이디 타입 변경

--- backend/src/main/java/io/watssuggang/voda/pet/dto/res/ItemResponse.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/res/ItemResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b82a2949b18183d95a45555d19b3491c11aaf2cae065bb70baf4c612f354e8
-size 263
+oid sha256:59dffd37a110d5322e2a015219d4aff3f201df9cf7326bd5739b219b195da0d8
+size 266
