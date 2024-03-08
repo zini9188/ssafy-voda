626a7a38bbff83723df173eaf28368a68c1a3960
zqwz <zqwz@naver.com>
2024-03-08 15:32:45 +0900

    feat: 먹이, 효과 클래스 추가

---
+++ backend/src/main/java/io/watssuggang/voda/pet/domain/Effect.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a49e7819b97835d768a20dedaaa58bd57dc6701449c35ab2aee6b94a9654f0b
+size 80
---
+++ backend/src/main/java/io/watssuggang/voda/pet/domain/Food.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13f9e2356d60cba83aa524a5170e62cf30a09277a627a79e2e85c94064b3b3e8
+size 77
