1f77f1a99646b280b9d6c25f015409b2d5f4e6ad
이수민 <suminjeff@gmail.com>
2024-03-11 17:40:29 +0900

    feat(mypage): Switch branch sw.sj

--- frontend/voda/dev-dist/sw.js
+++ frontend/voda/dev-dist/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b280ea4f8a29a8c3ef54c306ad14cce798018672db92ad31a350a4020e25408
-size 2839
+oid sha256:41e0218a452f84f2a3fb10b31f253ed834bb1e9cf7bc41558249cdadaa298580
+size 2838
