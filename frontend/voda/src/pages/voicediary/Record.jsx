c5b441e9bd4ef438937230a36680cfa7e9a3a2ae
김병주 <qudwn2941@naver.com>
2024-03-11 17:47:00 +0900

    Chore: Create voice diary record page

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83a34384da7a65b61603abbcf575664442ba39364932c34ebdaa18228a44cfe3
-size 143
+oid sha256:2e7deb2954fb156450db8c799953f59c3f7fbca079fec64df0b030cdece492c1
+size 2286
