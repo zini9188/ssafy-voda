2b830714566fa0f8d0572c1df219fe5adffdc19e
zqwz <zqwz@naver.com>
2024-03-12 10:53:04 +0900

    fix: Emotion label 접근제어자 변경

--- backend/src/main/java/io/watssuggang/voda/common/enums/Emotion.java
+++ backend/src/main/java/io/watssuggang/voda/common/enums/Emotion.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:090f1c1b68ec37da76a314ff752b2eb55c178ffd1481b2f97a2a4c514614fda2
-size 265
+oid sha256:4e23986d89b3a64bf7621cbacebeba27fa02a9309dc4996b12f9197bed8cefab
+size 266
