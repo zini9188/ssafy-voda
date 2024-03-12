d25c865fa06ee465a8b65e69e0d772d077e40bdc
zqwz <zqwz@naver.com>
2024-03-12 13:15:43 +0900

    refactor: 펫 상태 용어 변경

--- backend/src/main/java/io/watssuggang/voda/common/enums/PetStatus.java
+++ backend/src/main/java/io/watssuggang/voda/common/enums/PetStatus.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd0ee2f1c2cc4cedf43f25c8576a3f07d2fed3555823b5fcb7b9faa74bbf8043
-size 583
+oid sha256:a94614b31c2efd0d73c6ba45087517a492a7ec31db8645f882e1d2fa25fc3c3e
+size 582
