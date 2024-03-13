71ef5a13130b62fc5edaa012b8bc02e83fb30168
zqwz <zqwz@naver.com>
2024-03-13 13:03:47 +0900

    feat: BaseEntity Getter 추가

--- backend/src/main/java/io/watssuggang/voda/common/domain/BaseEntity.java
+++ backend/src/main/java/io/watssuggang/voda/common/domain/BaseEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c282f22c428787c5a3d5bdc68e760268d2e2cec9084b72ecfa367cc8c8ae76c
-size 850
+oid sha256:f02a745b6bb8ecafa891b7fa702e3287a961d55433bcf2666852892071f6f17b
+size 880
