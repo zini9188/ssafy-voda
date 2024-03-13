a675fb463bf96424614e18ae61af3e52c81d86ad
sikutisa <ilgooinkorea@gmail.com>
2024-03-13 10:41:41 +0900

    feat: add deleteUser entry point
    Related to: S10P22A104-133

--- backend/src/main/java/io/watssuggang/voda/member/controller/MyPageController.java
+++ backend/src/main/java/io/watssuggang/voda/member/controller/MyPageController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4506e20a08eb6723c28780ab464e3672cbd9e08f4953c91b1a0fc46622ec7127
+oid sha256:7d23742d87a3b7710eb63701b7f2f89349ed07aa0ddd0c9cdea163086574c475
 size 1974
