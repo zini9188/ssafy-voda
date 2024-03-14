85fded31f6a7681b6f5b23f93b53f2a7b68933a2
zqwz <zqwz@naver.com>
2024-03-14 14:15:23 +0900

    fix: API BASE URI 변경

--- backend/src/main/java/io/watssuggang/voda/pet/controller/ItemController.java
+++ backend/src/main/java/io/watssuggang/voda/pet/controller/ItemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63b5b1cc1b8620f47946a757d2a05396d99a619a4211c106c825091c11429a4a
-size 1390
+oid sha256:bcff817dd62bb17e8619a982c65bb2de3903cb1c1c243b7c969cbfa1d007d115
+size 1383
