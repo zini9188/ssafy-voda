30feb31fa7e5f4aa21468af7e6e0b62dd04a8b6f
zqwz <zqwz@naver.com>
2024-03-14 14:06:33 +0900

    fix: 접근제어자 변경

--- backend/src/main/java/io/watssuggang/voda/pet/dto/req/PetUpdateRequest.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/req/PetUpdateRequest.java
@@ -1,12 +1,3 @@
-package io.watssuggang.voda.pet.dto.req;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class PetUpdateRequest {
-
-    public String name;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:78006025b422f36f300117d070bb7b5dde4ead27e1da777c4c8dc5f84f2fd03f
+size 186
