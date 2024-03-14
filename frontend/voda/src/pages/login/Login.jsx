8f0f688ac90cd9c87c17d02c43434af6a0d3de28
이수민 <suminjeff@gmail.com>
2024-03-14 09:28:53 +0900

    feat(login): Fix login redirection page

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:433856b415dc83dd7b184931a48117516d57b443e8adc9443ae0ef0bf9e448bd
-size 2574
+oid sha256:13fe4dc7a2a7c869794259e8603a80b4aaa9e8c0fcbe9a0955378ecc444cf612
+size 2388
