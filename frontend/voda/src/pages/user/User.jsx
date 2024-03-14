3975c0d64567d188fcb725c14b1dbe0d77f22ca6
이수민 <suminjeff@gmail.com>
2024-03-14 09:55:01 +0900

    feat(auth): Add refreshToken test codes

--- frontend/voda/src/pages/user/User.jsx
+++ frontend/voda/src/pages/user/User.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db764f45d3a1b405eeb5d17a3192d750b0b84ff7058fd27e3f8ac4c8194fd37d
-size 1535
+oid sha256:e24af3f0d7052159b08c9c8822598dfb07dc16fb89dd95f98f391208910383e4
+size 1868
