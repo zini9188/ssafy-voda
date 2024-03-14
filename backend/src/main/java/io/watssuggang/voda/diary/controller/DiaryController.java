eb9e6a2a7eb9b9bdd4ee876bfd649c72976a22cb
전소민 <goflvhxj96@naver.com>
2024-03-14 16:01:13 +0900

    feat: jenkins test

--- backend/src/main/java/io/watssuggang/voda/diary/controller/DiaryController.java
+++ backend/src/main/java/io/watssuggang/voda/diary/controller/DiaryController.java
@@ -1,24 +1,3 @@
-package io.watssuggang.voda.diary.controller;
-
-import io.watssuggang.voda.diary.dto.res.DiaryChatResponseDto;
-import io.watssuggang.voda.diary.service.DiaryService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/diary")
-@RequiredArgsConstructor
-public class DiaryController {
-
-  public final DiaryService diaryService;
-
-  @GetMapping("/init")
-  public ResponseEntity<?> init() throws Exception {
-    DiaryChatResponseDto result = diaryService.init();
-    return ResponseEntity.ok(result.getContent().get(0).getText());
-  }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a37635d09d86dee934ad6365b488c794221165b3f8a37735c7eb24850310379
+size 818
