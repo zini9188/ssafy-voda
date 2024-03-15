e8b381dfe92fe2bd05712abf520725dca9c07d56
김진호 <lantern50@naver.com>
2024-03-15 09:59:16 +0900

    Refactor: Auth, Member exception 수정

--- backend/src/main/java/io/watssuggang/voda/common/security/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/io/watssuggang/voda/common/security/filter/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9490ca586ec01634e37399cb00bf13565785615eae34a1cba9b0626ea7ad3abd
-size 3166
+oid sha256:48202c227ed70df8e85af9fa0b83a680c7da241893db531b970d53db4c41e3d1
+size 3087
