2875f64588885db7ddd1db9a706ac4f39a0d6c65
여일구 <ilgooinkorea@gmail.com>
2024-03-18 12:02:22 +0900

    Update Jenkinsfile

--- backend/src/main/java/io/watssuggang/voda/member/controller/MemberController.java
+++ backend/src/main/java/io/watssuggang/voda/member/controller/MemberController.java
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d17142d2220dd788ab91c696ac3f9b1d8d44aa8f395adfdf53938ff1f8ab8f5
-size 1278
+package io.watssuggang.voda.member.controller;
+
+import io.watssuggang.voda.common.security.annotation.*;
+import io.watssuggang.voda.common.security.dto.*;
+import io.watssuggang.voda.member.dto.req.*;
+import io.watssuggang.voda.member.service.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody SignUpRequest signUpRequest) {
+        log.info("회원가입");
+        Integer memberId = memberService.signUp(signUpRequest);
+
+        if (memberId == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+
+    }
+
+    // 마이페이지 유저 정보 GET
+    @GetMapping("/ilgoo")
+    public ResponseEntity<SecurityUserDto> ilgoo(@CurrentUser SecurityUserDto securityUserDto) {
+
+        return ResponseEntity.ok(securityUserDto);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> removeUser() {
+        // TODO 회원탈퇴 구현
+        return ResponseEntity.ok().build();
+    }
+}
--- backend/src/main/java/io/watssuggang/voda/member/dto/DailyStaticDto.java
+++ backend/src/main/java/io/watssuggang/voda/member/dto/DailyStaticDto.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e6164ee31b5320fbef8ac884b714c8b5e1c848c946e25fb3fd8dc2ee8fcd7d0e
-size 234
+package io.watssuggang.voda.member.dto;
+
+import io.watssuggang.voda.common.enums.Emotion;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class DailyStaticDto {
+
+    private Emotion emotion;
+    private String summary;
+}
