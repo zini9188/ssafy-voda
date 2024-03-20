0e37fec401fc908e829bbf5fa6b8be0060d3018c
김진호 <lantern50@naver.com>
2024-03-20 10:02:22 +0900

    Fix: 로그인 redirect URL 수정

--- backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
+++ backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ef1b058a2b3e0127944b6b882bbecc2a5a2a406a1130a13a159f5f39d21e568
-size 638
+oid sha256:182044d98cbdb6c449a516204d38428f389e24d986b58389cdc8d377e0cbf316
+size 637
