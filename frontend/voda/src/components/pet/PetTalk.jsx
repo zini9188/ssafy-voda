11004a9a57aba309f0c9b24a64180de3ff480a82
여일구 <ilgooinkorea@gmail.com>
2024-03-20 18:02:22 +0900

    Update frontend/voda/src/components/pet/PetTalk.jsx

--- frontend/voda/src/components/pet/PetTalk.jsx
+++ frontend/voda/src/components/pet/PetTalk.jsx
@@ -1,19 +1,3 @@
-import React from "react";
-import styled from "styled-components";
-
-const Chatbox = styled.div({
-  borderRadius: "2rem",
-  backgroundColor: "#686868",
-  width: "70vw",
-  color: "white",
-  textAlign: "center",
-  padding: "1rem",
-});
-
-export default function PetTalk(props) {
-  return (
-    <>
-      <Chatbox>일기 써 줘.</Chatbox>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6479f4be8366d0969147bd71ded336f7b29686777b2226f866ddf2020510f5ae
+size 354
