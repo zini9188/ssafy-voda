614dc6e8139406502bc040805acb99bd55f28bb9
김진호 <lantern50@naver.com>
2024-03-20 10:00:45 +0900

    Fix: 로그인 redirect URL 수정

--- backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
+++ backend/src/main/java/io/watssuggang/voda/common/security/handler/OAuth2FailureHandler.java
@@ -1,18 +1,3 @@
-package io.watssuggang.voda.common.security.handler;
-
-import jakarta.servlet.http.*;
-import java.io.*;
-import org.springframework.security.core.*;
-import org.springframework.security.web.authentication.*;
-import org.springframework.stereotype.*;
-
-@Component
-public class OAuth2FailureHandler implements AuthenticationFailureHandler {
-
-    // 인증 실패시 메인 페이지로 이동
-    @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException exception) throws IOException {
-        response.sendRedirect("http://localhost:5173/login");
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ef1b058a2b3e0127944b6b882bbecc2a5a2a406a1130a13a159f5f39d21e568
+size 638
