9c96d7f12fd8af7693012aeecbb30fcbb5e14b21
여일구 <ilgooinkorea@gmail.com>
2024-03-21 10:57:43 +0900

    Update frontend/voda/src/pages/login/Login.jsx

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66b05103a174ca6afbcdfd00cfb18f9286d219c71b032930d523d0bc7aaa49bd
-size 2486
+oid sha256:608eaebf6a4f698d60dc474434fb8397057d44b3fa2f5ec1f9b4c64fb075acbc
+size 2475
