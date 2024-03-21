74b8c3dca830f4dfaf4fce44e59fe5230069cd9a
sikutisa <ilgooinkorea@gmail.com>
2024-03-21 14:26:31 +0900

    get response test

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66b05103a174ca6afbcdfd00cfb18f9286d219c71b032930d523d0bc7aaa49bd
-size 2486
+oid sha256:990ec09f9dc0f5f3771bb9f977dbda6bb8f02323369348ee3fdb10b17e51a19a
+size 2866
