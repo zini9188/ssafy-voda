b7960715f606dafb24244a6d463f4065a94d78b1
이수민 <suminjeff@gmail.com>
2024-03-22 17:06:41 +0900

    feat(login): Test login test cache

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7ee45b8261ff064bf0c77a577db6bab1876ae92c97ead14246cc0df7575d419
-size 2755
+oid sha256:6a46ce34ee4c77680c474cf648bfa78bc7f8b08d40469a237c56e0b66653899d
+size 2607
--- frontend/voda/src/services/auth.js
+++ frontend/voda/src/services/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1b6c8ddd51742b554077787a37a13fb915db32996a09ca99eb542ea7a171dd2
-size 1554
+oid sha256:69defb159be508e47046e477d8ad6587e296464e03905608ae6f9eea25e541c7
+size 2895
