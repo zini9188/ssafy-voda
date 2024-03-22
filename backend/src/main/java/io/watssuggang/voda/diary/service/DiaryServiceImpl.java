28b8d88b8a75286898bf63067ecf68cc60a7780f
zqwz <zqwz@naver.com>
2024-03-22 14:49:42 +0900

    feat: 칼로 이미지 요청 로직 추가

--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:530bf7a55bbb380368ec2c109e46cff3089bddecf0d2d486a23c093a45dee32e
-size 6151
+oid sha256:d4cf6c5885d2ccfd8085ac4fc4f055e5a4ce23a55a6b6748dc73ba66b06ba472
+size 6568
