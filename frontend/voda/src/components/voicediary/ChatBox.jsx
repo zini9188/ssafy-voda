05e5db98a04c762a4901ddf5d1019d126c1aa0a9
김병주 <qudwn2941@naver.com>
2024-03-22 09:32:32 +0900

    fix: voicediary components

--- frontend/voda/src/components/voicediary/ChatBox.jsx
+++ frontend/voda/src/components/voicediary/ChatBox.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c0b1bdb067225b11866b9e3d4fcadbd4e5759800ccb35c6ec934dc0c538dc57
-size 2574
+oid sha256:166dde519641d751cff55bb055413a1677eaf875a8d07864dbda5aa883b222af
+size 4469
