8d9c433143ac6222744816ba71849db94e716fe3
김병주 <qudwn2941@naver.com>
2024-03-22 09:29:02 +0900

    fix: checking chat & voicediary api connect

--- frontend/voda/src/pages/voicediary/CheckChat.jsx
+++ frontend/voda/src/pages/voicediary/CheckChat.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:374779730908e17b6e3ffa31b019b4563d79eb6817fbe23a0b4f1a2ae06f7de0
-size 1805
+oid sha256:cc7731e402e11818cf62408477d85f9b339a3e61620a395f6ae3cddd0917bb80
+size 1797
--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ead1a92b83eaaa14a0e8202b34c30fe37c72e2316db43bb7ef71c8853452ad85
-size 8207
+oid sha256:a704c277d05b8cada31f69bd9b5c395fe158675672f53a76e9f82bdee85107a4
+size 9197
