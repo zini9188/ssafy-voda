083899a90625b3334268eb53f64514c273eec4c1
이수민 <suminjeff@gmail.com>
2024-03-22 16:09:59 +0900

    feat(login): Login test

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ea5fad361369ad9de13e5ab352f55756fd02ba03b03b0a1c948a12bed6b140a
-size 3102
+oid sha256:18295547f8334ae329d2e7ac8fd8ce8e6d39e75fb139a14fe71377be6a08f49b
+size 2709
