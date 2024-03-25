b0d8a9f1dd195dd7589692a80f173c7794fa8eaa
sikutisa <ilgooinkorea@gmail.com>
2024-03-25 17:39:43 +0900

    fix: remove DiaryNotCreatedException

--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e34e2bfbd2d37355a2bcd0dacdf2d0c1b293c51ad434a078b2d9f34b17f6b98
-size 12389
+oid sha256:43d7e0dfbe535359c843ef150237cb3fe795e194f813c9a1f285abd229247dab
+size 12379
