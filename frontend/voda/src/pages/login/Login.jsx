9bf2d2a11d66a2de87de000129891859d7286793
이수민 <suminjeff@gmail.com>
2024-03-25 12:42:25 +0900

    feat(calendar): Login logic reverted to original (for local test)

--- frontend/voda/src/pages/login/Login.jsx
+++ frontend/voda/src/pages/login/Login.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a46ce34ee4c77680c474cf648bfa78bc7f8b08d40469a237c56e0b66653899d
-size 2607
+oid sha256:c0c1ea7634266a9dc89b127bd107cb947af089a4b09980daa56cddd599e2da5e
+size 2249
