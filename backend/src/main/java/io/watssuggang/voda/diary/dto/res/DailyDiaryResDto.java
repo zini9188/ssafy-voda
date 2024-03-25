720a5ce7f449cc80f6e36a1dc5ab6654f401a54c
sikutisa <ilgooinkorea@gmail.com>
2024-03-25 16:20:37 +0900

    refactor: fix DailyDiaryResDto

--- backend/src/main/java/io/watssuggang/voda/diary/dto/res/DailyDiaryResDto.java
+++ backend/src/main/java/io/watssuggang/voda/diary/dto/res/DailyDiaryResDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:978dc9bdd3006d03e353f0d446be39717bac8462df0707ce10157dedd68a955e
-size 1006
+oid sha256:36097b573fda7b72e61f8f2f017ff3514338de529c5400c4bf7453f36d5d7bf5
+size 1047
