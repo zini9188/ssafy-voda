ed530437a4839eb9f1fc9390ada6772c626b4e04
전소민 <goflvhxj96@naver.com>
2024-03-25 17:22:10 +0900

    fix:종료조건contains로 변경

--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:779ee0cfbc019625a65007a3e9037100a9f84d8416db5f83bddd9f928f331a9c
-size 12128
+oid sha256:8334d76548ac223db44c0f71ac33ebe8da9dce4e272f3c35aa0dd6bb6ef821e1
+size 12130
