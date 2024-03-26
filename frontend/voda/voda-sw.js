d944117206e4a6c6b9704e5260e707d9d53c7eff
김진호 <lantern50@naver.com>
2024-03-26 13:56:55 +0900

    fix: sw.js 수정

--- frontend/voda/voda-sw.js
+++ frontend/voda/voda-sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8eb1e76193604ed565f16d50eff5844c1920fec0a4633b27c0a3e627e67ef8e
-size 1253
+oid sha256:3e41a2a615ec9554d368b53462bfb8c70ff65a4d0fce505edbc6adf79e9753a3
+size 1410
