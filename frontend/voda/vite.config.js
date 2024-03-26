2290d4e3ca40729f0221dd9d0dfafd646e3845ca
김진호 <lantern50@naver.com>
2024-03-26 13:51:31 +0900

    fix: vite.config 수정

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af217e8821ad312d211777f52dcc7f58f6227eac6f91495b127dd878a68e41ed
-size 808
+oid sha256:3a24f001aa6dd70a1065bc1811774dc05e3ae72f513c6810b2f62a6a622796de
+size 868
