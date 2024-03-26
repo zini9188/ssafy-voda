18122f48e3211358aeb5122d772b905dcc19b3d8
zqwz <zqwz@naver.com>
2024-03-26 10:02:23 +0900

    fix: 사용중인 아이템만 보이도록 변경

--- backend/src/main/java/io/watssuggang/voda/pet/dto/res/PetHomeResponse.java
+++ backend/src/main/java/io/watssuggang/voda/pet/dto/res/PetHomeResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7cf14092c754d5e906c6b3a58e2e67e2dca86990751f44296e606095dfe267f
-size 505
+oid sha256:0ff01c83c39f7aa7cf95dc90a34c6535af9a341ad0d27b4627d3c46951e69d6d
+size 543
--- backend/src/main/java/io/watssuggang/voda/pet/service/OwnService.java
+++ backend/src/main/java/io/watssuggang/voda/pet/service/OwnService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22f650a947d1aefb3fe72280e76d069f87dfaf172f11c0ef5784c11a0146f75f
-size 2240
+oid sha256:e1167967d424f849b6505a3309cf9cd37ae72b2c1ed7ed523a7659a4bd9e0e5f
+size 2567
