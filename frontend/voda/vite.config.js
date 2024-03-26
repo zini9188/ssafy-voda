43b2a6af76fc239c53391ec4b3922329e914394e
김진호 <lantern50@naver.com>
2024-03-26 14:28:37 +0900

    fix: 원복

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a3d7330ffea2b2afc77dfbc95505d2dcbad328d203c5f6c8d2be809bae6e19
-size 911
+oid sha256:9a9a56be7c2d343573b9b7a114c42acd363ff85cf2acdcc084db633c25474fea
+size 1195
