99ab40c5e31d5d3ed6799bfda1f130b0d59c47e8
zqwz <zqwz@naver.com>
2024-03-27 15:52:35 +0900

    fix: 서비스워커 환경파일 변경

--- frontend/voda/vite.config.js
+++ frontend/voda/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92aa8e5f4c32415469d94a8db24ae1367341a66512a9ce6981f01d09017fab96
-size 1320
+oid sha256:f0444858919e922bcd31eee3207f35807d06b6f841becce44f3dc72a731513fb
+size 1292
