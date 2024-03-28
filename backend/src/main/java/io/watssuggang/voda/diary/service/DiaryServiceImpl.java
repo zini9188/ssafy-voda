2267673b6fef3eefc65bc9a8abb468b6f702a976
전소민 <goflvhxj96@naver.com>
2024-03-28 16:23:43 +0900

    fix: prefix 정리

--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba9124ed95dbf4b6eb6867c739257bb2e2985d8679ff4bb9b5cb408bad6aa70f
-size 16264
+oid sha256:5449659f2d6834ea8f5dd6028b18714290a3f35c4db89251762be9080e840133
+size 16241
