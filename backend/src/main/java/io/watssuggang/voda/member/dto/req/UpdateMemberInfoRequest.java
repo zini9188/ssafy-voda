046cc6ff04475ce64d5715f1636674ddcb7d8b0b
zqwz <zqwz@naver.com>
2024-03-28 13:20:14 +0900

    feat: 회원 닉네임 수정 유효성 검사 추가

--- backend/src/main/java/io/watssuggang/voda/member/dto/req/UpdateMemberInfoRequest.java
+++ backend/src/main/java/io/watssuggang/voda/member/dto/req/UpdateMemberInfoRequest.java
@@ -1,12 +1,3 @@
-package io.watssuggang.voda.member.dto.req;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-public class UpdateMemberInfoRequest {
-
-    private String newNickname;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:05a7e8e918d5bfabd58773850f5eabd349849f7b7790a1679083bec42780c44d
+size 506
