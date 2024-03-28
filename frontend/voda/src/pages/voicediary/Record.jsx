dc4a6025a0b2d51dd4879310376b8f734b9b99e6
김병주 <qudwn2941@naver.com>
2024-03-28 13:14:16 +0900

    Feat: diary delete when move to other url

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1dc51d2f462037b5fbbb08a45ae9b992c3676cb58d4e1cd1abb6162860057e1
-size 9394
+oid sha256:1e04ac378833b8121dcfc7d3b15d96407160b6b9ec6143aef6e47109043786ee
+size 9484
