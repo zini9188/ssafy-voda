db954cacfd741350eda5226b85a13b289980822f
김병주 <qudwn2941@naver.com>
2024-03-29 11:21:41 +0900

    Fix: editAllow logic update

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eebdd6bcc8b3da7f939e511a0d5219ede9e02baa8e3f7fc7abdcff0283b8771b
-size 11961
+oid sha256:5f9e24defd2142d4d082c15fa0d755f6ed4ada1d0d9f19f4b00dfef82d76cc0b
+size 11983
