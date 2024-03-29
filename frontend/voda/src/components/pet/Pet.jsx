5c4f9ee24bc23266b16fce54570394c23bdc1011
이수민 <suminjeff@gmail.com>
2024-03-29 16:13:21 +0900

    feat(pet): Add pet touch action

--- frontend/voda/src/components/pet/Pet.jsx
+++ frontend/voda/src/components/pet/Pet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a68dae1df1ad98fd8d0b2f890aa98540b0516c0684d1eac27f44cb252f5c7b97
-size 9860
+oid sha256:44348fd1a503f9a654db69a268f21d55d439767edd224d92bd6723a9132d5c7b
+size 9932
