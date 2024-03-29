4c484e50e37460dcdcb38ea005e7aebbbe03dce5
zqwz <zqwz@naver.com>
2024-03-29 15:38:08 +0900

    fix: 펫 대화 오류 해결

--- backend/src/main/java/io/watssuggang/voda/pet/service/PetService.java
+++ backend/src/main/java/io/watssuggang/voda/pet/service/PetService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e53c17a298a99d543927f42c15580fc65d34ae59974e9bef6401bf786ec3aa8d
-size 8654
+oid sha256:eac556121289ccbbf298bf211c5c086ec11d0d639d1dd670eb777bb7c9dcec48
+size 8861
