d4c669a178743d74c94651b7befc96ea694ee171
김병주 <qudwn2941@naver.com>
2024-03-29 15:29:12 +0900

    Fix: api request change

--- frontend/voda/src/components/common/PWAInstallButton.jsx
+++ frontend/voda/src/components/common/PWAInstallButton.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec43cb3c7d98b2e94ce972c991828c68156a81f20d327a6b3000c3847c763a1c
-size 1583
+oid sha256:678f226abe86633a363fa4f0eb744d5aa9774d6511e61ec290064ecf006582d0
+size 1557
--- frontend/voda/src/services/voicediary.js
+++ frontend/voda/src/services/voicediary.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5100ad603ca5089b23d2791d74a3ad3619a6605a5f7a3df5bfe3fde491d015f
-size 1955
+oid sha256:fb3cd961756901c535734f198cacc37e3240d2904b01da7da0afbf76499f658c
+size 2013
