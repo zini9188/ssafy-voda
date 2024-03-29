6cc07c8d7d8f10a63748e85ef0e57ff6cb015d0a
이수민 <suminjeff@gmail.com>
2024-03-29 14:52:11 +0900

    feat(pet): Add pet talk

--- frontend/voda/src/pages/pet/PetPage.jsx
+++ frontend/voda/src/pages/pet/PetPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:536fe118d55c2d3dcb8d20082d7666b2c61b8a990e08014aceb8e8bac986f159
-size 2840
+oid sha256:cdec6345db48192022da1c3572252b4bae5bd63215e605eb34ade670bcc890ea
+size 2804
--- frontend/voda/src/services/pet.js
+++ frontend/voda/src/services/pet.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8f8290f7c5ef68a200a2fea0f48ddd3b957e91bbd7a699e914af892d39641b3
-size 1255
+oid sha256:13c1a03b2d8f89b14c3977fcd5de2785d7138d6051d591c5e242ef107fddfc73
+size 1266
