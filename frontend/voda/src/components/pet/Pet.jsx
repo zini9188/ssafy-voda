2e886c3e7d70e6537a679d09c60be03f9f3fc02f
이수민 <suminjeff@gmail.com>
2024-04-01 12:53:13 +0900

    fix: Fix levelup logic

--- frontend/voda/src/components/pet/Pet.jsx
+++ frontend/voda/src/components/pet/Pet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:305fe4b1a702b7cbe649d02fa4b412539d2100662e529356e69faf774610dcd9
-size 9943
+oid sha256:99e7a59f3d0d87d916045897fb9b70f15e24badf2de28d01d6f90738c829af14
+size 9942
