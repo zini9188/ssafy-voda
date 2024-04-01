054a375b5ae3b31c6c7784aa9b2077ec12ecc0fd
이수민 <suminjeff@gmail.com>
2024-04-01 09:21:46 +0900

    fix: Fix calendar style

--- frontend/voda/src/pages/calendar/CalendarPage.jsx
+++ frontend/voda/src/pages/calendar/CalendarPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97d8a95e46f82b440ea2ace1225024096512df0b8e177ad1183874c3ca44037f
-size 428
+oid sha256:6ed5f5c8095ee4564ca08aabfe6428c721fed51944a69dcb32523bdf02233a55
+size 500
