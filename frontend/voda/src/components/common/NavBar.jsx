149e59447cba27e33b11c77095e8f0565ba648b0
김병주 <qudwn2941@naver.com>
2024-04-01 17:21:04 +0900

    Fix: confirm message change

--- frontend/voda/src/components/common/NavBar.jsx
+++ frontend/voda/src/components/common/NavBar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10117781b509fb56e769e4fdcd4ca60a5dd30ede46359feb302bc34937e3ee98
-size 3887
+oid sha256:c6db9870d2d8ab3aac12e727e08cdd5948c12229bffc6788e3143ab33dfe6a31
+size 3910
--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8572040dd76b25cf9ebab97b47914da4c4c84ad1265b64977a3008849a86fcae
+oid sha256:0be8c1c016425cf27e95f1ae64b6f28b9c847c5aa706470d7df3060188ebcab1
 size 13139
