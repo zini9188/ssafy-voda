250a95e924899bae94f81ecd11a387d5972a73bc
전소민 <goflvhxj96@naver.com>
2024-04-01 09:59:40 +0900

    fix:answer 프롬프트 수정

--- backend/src/main/java/io/watssuggang/voda/diary/util/PromptHolder.java
+++ backend/src/main/java/io/watssuggang/voda/diary/util/PromptHolder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33438570825a6dcc3747bd35a8de24adee04f01611b1a7df16691daad84bd547
-size 1685
+oid sha256:a4b03a25eedf750661482ecc2d00524a4b528c390813245d92b07ee198c6350c
+size 1704
