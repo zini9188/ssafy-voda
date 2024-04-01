f5066929e13cc24664a3d7444371fbe04d1a70eb
김병주 <qudwn2941@naver.com>
2024-04-01 09:32:18 +0900

    Fix: chatting box height

--- frontend/voda/src/components/voicediary/ChatBox.jsx
+++ frontend/voda/src/components/voicediary/ChatBox.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adfcc18ce66022ad4e20fafc6d39609b1cf46b6902cfb120d7832501a9b336e0
-size 2038
+oid sha256:9d7df0b031854bfca714d696d04c74bc1a579b307352cfe1f53c8baefc29df0e
+size 1976
