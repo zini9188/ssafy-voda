da2f29f37322fe4686a276b3b8e9c80441a5bf24
이수민 <suminjeff@gmail.com>
2024-04-01 17:37:15 +0900

    fix: Fix user report pet bug

--- frontend/voda/src/components/user/AnalysisPet.jsx
+++ frontend/voda/src/components/user/AnalysisPet.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c9d03b3b114361ab11c4df745c922ee652aa8741f2438192afd568d6de03ccc
-size 1416
+oid sha256:f8f2067e076ee78d8c4844f5e96897dd1ca8aa3c589996056efe42656c6f5038
+size 2000
--- frontend/voda/src/pages/user/UserReport.jsx
+++ frontend/voda/src/pages/user/UserReport.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b1ff5c80f412dabea6e34a43d39a3b887c83d068f6b185421d25ca899739a74
+oid sha256:acfb6073a7a76ce678a2d77860989c0392c0448dc667886205ff850dc60e93b5
 size 837
