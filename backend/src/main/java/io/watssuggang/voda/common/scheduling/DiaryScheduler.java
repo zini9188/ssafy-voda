06d700556f0f3e645994bd730a300769adebac04
김진호 <lantern50@naver.com>
2024-04-01 13:02:53 +0900

    Fix: scheduler 수정

--- backend/src/main/java/io/watssuggang/voda/common/scheduling/DiaryScheduler.java
+++ backend/src/main/java/io/watssuggang/voda/common/scheduling/DiaryScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d9390b691c5ae044f7fdb6b6b7a1fac19f19498f8cbaddabc9387cd03156da8
-size 797
+oid sha256:496be2924c81addc9512c7a9bc69b678517f55cf000f6b298fce4f7b9ac1acff
+size 741
