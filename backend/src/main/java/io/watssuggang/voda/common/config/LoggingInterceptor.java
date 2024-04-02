36e44e6098c18072dbcfff5a53f4f3240e7bcc57
zqwz <zqwz@naver.com>
2024-04-02 14:10:59 +0900

    feat: 쿼리 시간 계산 인터셉터 및 쿼리 개수 측정 기능 추가

---
+++ backend/src/main/java/io/watssuggang/voda/common/config/LoggingInterceptor.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d035e3da7334d8beb256f2c980136d44a908966df8d7e8b8c4afefcfd1b3987
+size 1848
---
+++ backend/src/main/java/io/watssuggang/voda/common/config/WebConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1501fe6794912f8b9477d801068350528fc88be249eba101e1a240446bbcfa1d
+size 615
