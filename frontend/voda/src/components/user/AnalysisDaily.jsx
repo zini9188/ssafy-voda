89888db2184e0f2652318bdd22f08e35e16b8a9a
김병주 <qudwn2941@naver.com>
2024-04-02 16:45:01 +0900

    Fix: slider dots color

--- frontend/voda/src/components/user/AnalysisDaily.jsx
+++ frontend/voda/src/components/user/AnalysisDaily.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:340d5b97549b037fe644f0ae2f7f5328a06cbb438df3cd70a735e9a527413643
-size 3545
+oid sha256:4572ba67dbcd935bf027ef6e9b31b39354cca28b7000b9c996c904179f1b4cb1
+size 3795
