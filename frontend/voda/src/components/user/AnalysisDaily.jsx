744cdbcafb6ee6ad96fa6dbecdad550a7b105712
김병주 <qudwn2941@naver.com>
2024-04-02 16:56:40 +0900

    Fix: daily analysis dots

--- frontend/voda/src/components/user/AnalysisDaily.jsx
+++ frontend/voda/src/components/user/AnalysisDaily.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4572ba67dbcd935bf027ef6e9b31b39354cca28b7000b9c996c904179f1b4cb1
-size 3795
+oid sha256:bdfb49d5a3af4bab869a3dc5583a6fb3812e1627ad36b6001102aa279d555088
+size 4132
