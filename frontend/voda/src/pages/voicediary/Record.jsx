35a4fb1d144525893b531ac818947ca58ce0f639
김병주 <qudwn2941@naver.com>
2024-04-02 15:24:17 +0900

    Fix: terminateRecording alert

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb9a60af06d215662906c1a3127868810593bb683ead73043c0975c99eeccdb
-size 14862
+oid sha256:bc792a1fa431b23058edab5d11f43a8b84d2a413a5c088b6410d07f2527d4728
+size 14989
