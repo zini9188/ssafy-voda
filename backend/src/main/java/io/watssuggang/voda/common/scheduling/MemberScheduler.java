07d6a7fae128da685503e9d9d6524ab32d650a23
sikutisa <ilgooinkorea@gmail.com>
2024-04-02 09:12:26 +0900

    fix: fix cron config

--- backend/src/main/java/io/watssuggang/voda/common/scheduling/MemberScheduler.java
+++ backend/src/main/java/io/watssuggang/voda/common/scheduling/MemberScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bddfe78ea9aa98ad8539b57a53acac5fc07d05744eb246cbacad37d8a2155893
-size 689
+oid sha256:85ba4461df2502a07b195f0c82e5d78104e5bc3f4ac92e87dcb40081c90a6831
+size 691
