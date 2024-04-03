0ee620ee70e94628c4f1e8c96eed0a5642f7cc75
이수민 <suminjeff@gmail.com>
2024-04-03 09:11:09 +0900

    fix: Delete console log

--- frontend/voda/src/services/api.js
+++ frontend/voda/src/services/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7760ca2c7ead40982d906bcd603aae2bf2e7c607eeff16a39b0c575c238364
-size 2488
+oid sha256:998e1e604bf00a3e8166750d52d88ef552605fe819d5e626fc91c9051e6609f4
+size 2456
--- frontend/voda/src/services/pet.js
+++ frontend/voda/src/services/pet.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c1a03b2d8f89b14c3977fcd5de2785d7138d6051d591c5e242ef107fddfc73
-size 1266
+oid sha256:4b6a8d6baa5f27f24f774998bfdf9e6b41bff04a9508c0d08d4cb7ceb364feb3
+size 1241
