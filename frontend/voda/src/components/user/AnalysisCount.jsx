c78239df97bd427547ee68cc9121df3dc2d32c05
김병주 <qudwn2941@naver.com>
2024-04-03 10:39:51 +0900

    Fix: decibel

--- frontend/voda/src/components/user/AnalysisCount.jsx
+++ frontend/voda/src/components/user/AnalysisCount.jsx
@@ -1,34 +1,3 @@
-import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 30% auto;
-`;
-const AnalysisCount = ({ report }) => {
-  const EMOJI_URL = import.meta.env.VITE_EMOJI_URL;
-  return (
-    <Container>
-      <div
-        style={{
-          textAlign: "center",
-          fontWeight: "bold",
-          fontSize: "1.4rem",
-          marginBottom: "5%",
-        }}
-      >
-        이번달 일기는 <br />총 {report.diaryCount}번 기록했어요
-      </div>
-      <img
-        src={`${EMOJI_URL}/Hand%20gestures/Writing%20Hand%20Medium-Light%20Skin%20Tone.png`}
-        alt="Writing Hand Medium-Light Skin Tone"
-        width="300"
-        height="300"
-      />
-    </Container>
-  );
-};
-
-export default AnalysisCount;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b221b37e50156284c098ce9a4b19453633fd389a741af0dca1374852ca8ad889
+size 824
