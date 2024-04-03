9837dc9283d56c15a1a6628749d8a8488393c87e
zqwz <zqwz@naver.com>
2024-04-03 16:33:58 +0900

    fix: 대화 목록 레디스로 분리

--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bdd49b27b2729540e4007204afd0770b235bd7cbaeb9878801546f52ae20f75
-size 17443
+oid sha256:64ed88af03720840d1d0c74e25b0d09121d14f2ec6b862bbe5bab331e1947420
+size 17740
