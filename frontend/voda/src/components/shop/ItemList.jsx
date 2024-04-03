ea81595a6231dfd9d0acc69dfb6a0f6eaa7f732c
이수민 <suminjeff@gmail.com>
2024-04-03 09:34:04 +0900

    fix: Fix darkmode shop

--- frontend/voda/src/components/shop/ItemList.jsx
+++ frontend/voda/src/components/shop/ItemList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9fd2bd364fc74d6348777b431cdffda52659b27029d3ae361508f576ce4b7d
-size 8524
+oid sha256:a36700b6a621c585fec65a672c09da8a96324f6a1c73d0f7f5532f8c99b5b8e1
+size 8970
