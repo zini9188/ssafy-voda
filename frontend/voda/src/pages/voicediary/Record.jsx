be11bfbb3851ac0e47b651caabc3db6c5532fcb4
김병주 <qudwn2941@naver.com>
2024-04-03 10:48:33 +0900

    Fix: recognition start

--- frontend/voda/src/pages/voicediary/Record.jsx
+++ frontend/voda/src/pages/voicediary/Record.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a554dc1a160e0b95197d08546aef47017d23ec2b714ab8ec377f48c9afe9aaf
-size 16474
+oid sha256:fcd7dacdd651c3dbc377369f19ba0548cdbdaa5fdecef5409bf1ec90c3e7f9da
+size 16449
