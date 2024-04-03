6b6182db919205dafa6a90dc4e80b64a6998d81a
sikutisa <ilgooinkorea@gmail.com>
2024-04-03 14:04:36 +0900

    fix: fix jpql

--- backend/src/main/java/io/watssuggang/voda/diary/repository/DiaryRepository.java
+++ backend/src/main/java/io/watssuggang/voda/diary/repository/DiaryRepository.java
@@ -1,30 +1,3 @@
-package io.watssuggang.voda.diary.repository;
-
-import io.watssuggang.voda.diary.domain.Diary;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface DiaryRepository extends JpaRepository<Diary, Integer>, DiaryCustomRepository {
-
-    @Query("SELECT COUNT(d) FROM Diary d WHERE d.member.pet.petId = :petId AND d.createdAt >= :createdAt")
-    Integer countDiaryByPetIdAndAfterToday(Integer petId, LocalDateTime createdAt);
-
-    List<Diary> findAllByMember_MemberId(Integer memberId);
-
-    @Query("SELECT d FROM Diary d WHERE d.member.memberId = :memberId AND MONTH(d.createdAt) = MONTH (CURRENT_DATE()) AND YEAR(d.createdAt) = YEAR(CURRENT_DATE())")
-    List<Diary> findAllByMemberAndCreatedAtAfterCurrentMonth(Integer memberId);
-
-    @Query("SELECT d FROM Diary d WHERE d.member.memberId = :memberId AND MONTH(d.createdAt) = :month AND YEAR(d.createdAt) = :year")
-    List<Diary> findAllByMemberAndCreatedAtGivenMonthAndYear(Integer memberId, Integer month,
-        Integer year);
-
-    @Query("SELECT d FROM Diary d WHERE d.member.memberId = :memberId AND DAY(d.createdAt) = :day AND MONTH(d.createdAt) = :month AND YEAR(d.createdAt) = :year")
-    List<Diary> findAllByMemberAndCreatedAtGivenDate(Integer memberId, Integer day, Integer month,
-        Integer year);
-
-    @Query("SELECT DATE(d.createdAt) FROM Diary d WHERE d.member.memberId = :memberId AND DATE(d.createdAt) < CURRENT_DATE() ORDER BY d.createdAt DESC LIMIT 1")
-    LocalDate findByMemberAndCreatedAtLast(Integer memberId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:74edb1c152e3e7c953f8a9f7c528701a3af284022740a4d041b89cc856e70590
+size 1613
--- backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
+++ backend/src/main/java/io/watssuggang/voda/diary/service/DiaryServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fcce9269006bb562cf98ec356dd1daebc7735ecf86caa5cb8ab5573a38c8b3d
-size 17442
+oid sha256:a38b552bc834995665f5cf7497bb7bcff467f88f1fc4093bb330d1c2e6c5834a
+size 17369
